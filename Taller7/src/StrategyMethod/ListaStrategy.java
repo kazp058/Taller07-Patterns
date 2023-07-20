918f046a0d5eeb02f794ded4a84c8c017c824a77
RommelZamora <razamora@espol.edu.ec>
2023-07-19 23:53:18 -0500

    Strategy Method

---
+++ Taller7/src/StrategyMethod/ListaStrategy.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package StrategyMethod;
+
+/**
+ *
+ * @author radri
+ */
+public class ListaStrategy implements VisualizarStrategy{
+
+    @Override
+    public void visualizar() {
+        System.out.println("Implementacion Lista Simple");
+    }
+    
+}
---
+++ Taller7/src/StrategyMethod/TableroStrategy.java
@@ -0,0 +1,19 @@
+package StrategyMethod;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author radri
+ */
+public class TableroStrategy implements VisualizarStrategy{
+
+    @Override
+    public void visualizar() {
+        System.out.println("Implementacion Tablero");
+    }
+    
+}
